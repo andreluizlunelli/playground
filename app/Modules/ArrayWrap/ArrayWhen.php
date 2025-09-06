675e721b77332f5c768ac0fa039c488b81aad9f8
André Luiz Lunelli <to.lunelli@gmail.com>
2025-09-06 19:55:12 -0300

    moved

--- app/Modules/ArrayWrap/ArrayWhen.php
+++ app/Modules/ArrayWrap/ArrayWhen.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace App\Modules\Macros\ArrayWrap;
+namespace App\Modules\ArrayWrap;
 
 use Closure;
 use Illuminate\Http\Resources\ConditionallyLoadsAttributes;
